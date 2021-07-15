b52a561db10a64b8cb3d4a9c8ec0949bf9d94158
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-07-15 21:50:54 +0900

    Add dummy

---
+++ slang/src/List/index.js
@@ -0,0 +1,10 @@
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+import 'react-dropdown/style.css';
+
+const option = ['slang1', 'slang2', 'slang3'];
+
+function List() {
+  return <Dropdown placeholderClassName='myPlaceholderClassName'></Dropdown>;
+}
+
+export default List;
