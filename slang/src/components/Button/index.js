7fe000259a742f8d904d2e74381f8cc0b8e61067
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-07-15 21:49:30 +0900

    Add Button

---
+++ slang/src/components/Button/index.js
@@ -0,0 +1,13 @@
+import './style.scss';
+
+function Button() {
+  console.log('Hi');
+  return (
+    <div className='div'>
+      <button className='button'>Sign Up</button>
+      <button className='button'>Sign In</button>
+    </div>
+  );
+}
+
+export default Button;
