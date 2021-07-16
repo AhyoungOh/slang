76a12b46dd3c407122718072c20cce1cbec86645
YujeongShin570 <58137372+YujeongShin570@users.noreply.github.com>
2021-07-16 22:44:41 +0900

    practice List folder

--- slang/src/List/index.js
+++ slang/src/List/index.js
@@ -1,6 +1,5 @@
 // import { DropdownButton, Dropdown } from 'react-bootstrap';
 // import 'react-dropdown/style.css';
-
 const options = ['slang1', 'slang2', 'slang3'];
 
 function List() {
@@ -8,24 +7,24 @@ function List() {
   const optionComponents = options.map((option) => {
     return (
       <li>
-        <a class='dropdown-item' href='#'>
+        <a class="dropdown-item" href="#">
           {option}
         </a>
       </li>
     );
   });
   return (
-    <div class='dropdown'>
+    <div class="dropdown">
       <button
-        class='btn btn-secondary dropdown-toggle'
-        type='button'
-        id='dropdownMenuButton1'
-        data-bs-toggle='dropdown'
-        aria-expanded='false'
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
       >
         Dropdown button
       </button>
-      <ul class='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
         {optionComponents}
       </ul>
     </div>
