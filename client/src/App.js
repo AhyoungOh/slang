5382496f56f5a6f65da9e56b67844de191faad5a
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-07-23 00:33:11 +0900

    list CRUD

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-//import logo from './logo.svg';
-// import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import SignUp from './SignUp';
 import AuthPage from './pages/Auth';
--- server/src/models/dictionary/index.js
+++ server/src/models/dictionary/index.js
@@ -9,7 +9,7 @@ const dictionarySchema = new Schema({
   },
   date: {
     created: { type: Date, default: Date.now },
-    edited: { type: Data, default: Date, now },
+    edited: { type: Data, default: Date.now },
   },
   is_edited: { type: Boolean, default: false },
 });
