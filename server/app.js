baabafa2aceea8d4a905345ab3d163d316571299
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-08-01 12:39:06 +0900

    port num

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 4000;
 const apiRouter = require('./src/routers');
 
 const userRouter = require('./src/middleware/user');
